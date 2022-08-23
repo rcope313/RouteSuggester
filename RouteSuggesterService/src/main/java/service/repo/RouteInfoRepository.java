3fb75d501d95734a8f42f3dbe51a9053fcb8730e
rachel <rachelcope.m@gmail.com>
2022-08-23 13:24:17 -0400

    Added class fields to RouteInfoRepository, as well as class constructor and subsequent getters.

--- RouteSuggesterService/src/main/java/service/repo/RouteInfoRepository.java
+++ RouteSuggesterService/src/main/java/service/repo/RouteInfoRepository.java
@@ -1,4 +1,22 @@
 package service.repo;
 
+import data.db.FourteenerDao;
+import data.db.TrailheadDao;
+
 public class RouteInfoRepository {
+    private final TrailheadDao trailheadDao;
+    private final FourteenerDao fourteenerDao;
+
+    public RouteInfoRepository(TrailheadDao trailheadDao, FourteenerDao fourteenerDao) {
+        this.trailheadDao = trailheadDao;
+        this.fourteenerDao = fourteenerDao;
+    }
+
+    public TrailheadDao getTrailheadDao() {
+        return trailheadDao;
+    }
+
+    public FourteenerDao getFourteenerDao() {
+        return fourteenerDao;
+    }
 }
