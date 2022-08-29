6178210192777566cbde8c7147a8f6f6ffc135eb
rachel <rachelcope.m@gmail.com>
2022-08-29 09:12:10 -0600

    Added empty POST methods in RouteInfoResources; the method paramaters & return type will change, as I will add different api request/response represesentations.

--- RouteSuggesterService/src/main/java/resource/resources/RouteInfoResource.java
+++ RouteSuggesterService/src/main/java/resource/resources/RouteInfoResource.java
@@ -1,10 +1,37 @@
 package resource.resources;
 
+import io.dropwizard.hibernate.UnitOfWork;
+import service.services.RouteInfoService;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/route_infos")
+@Path("/routes")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class RouteInfoResource {
+    private final RouteInfoService routeInfoService;
+
+    public RouteInfoResource(RouteInfoService routeInfoService) {
+        this.routeInfoService = routeInfoService;
+    }
+
+    @POST
+    @UnitOfWork
+    public void getRoutes(String body) {
+
+    }
+
+    @POST
+    @UnitOfWork
+    public void getSuggestedRoutes(String body) {
+        
+    }
+
+    public RouteInfoService getRouteInfoService() {
+        return routeInfoService;
+    }
 }
