70b56a6c807fb1eee31d459ac347631b630b49c9
rachel <rachelcope.m@gmail.com>
2022-08-29 11:50:29 -0600

    Updated method bodys of RouteInfoResource to include logic. Created empty methods in RouteInfoService for this logic.

--- RouteSuggesterService/src/main/java/resource/resources/RouteInfoResource.java
+++ RouteSuggesterService/src/main/java/resource/resources/RouteInfoResource.java
@@ -3,6 +3,7 @@ package resource.resources;
 import io.dropwizard.hibernate.UnitOfWork;
 import resource.api.ImmutableRoutesSearchCriteria;
 import resource.api.ImmutableRoutesSearchResults;
+import service.repo.RouteInfo;
 import service.services.RouteInfoService;
 
 import javax.ws.rs.Consumes;
@@ -10,6 +11,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/routes")
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,14 +26,22 @@ public class RouteInfoResource {
     @POST
     @UnitOfWork
     public ImmutableRoutesSearchResults getRoutes(ImmutableRoutesSearchCriteria body) {
-        return null;
+        List<RouteInfo> routes = routeInfoService.getRoutesBySearchCriteria(body);
+        return ImmutableRoutesSearchResults
+                .builder()
+                .addAllRoutes(routes)
+                .build();
     }
 
     @POST
     @UnitOfWork
     @Path("/suggest_by_weather")
     public ImmutableRoutesSearchResults getSuggestedRoutes(ImmutableRoutesSearchCriteria body) {
-        return null;
+        List<RouteInfo> routes = routeInfoService.getRoutesBySearchCriteriaAndWeather(body);
+        return ImmutableRoutesSearchResults
+                .builder()
+                .addAllRoutes(routes)
+                .build();
     }
 
     public RouteInfoService getRouteInfoService() {
--- RouteSuggesterService/src/main/java/service/services/RouteInfoService.java
+++ RouteSuggesterService/src/main/java/service/services/RouteInfoService.java
@@ -1,4 +1,17 @@
 package service.services;
 
+import resource.api.ImmutableRoutesSearchCriteria;
+import service.repo.RouteInfo;
+
+import java.util.List;
+
 public class RouteInfoService {
+
+    public List<RouteInfo> getRoutesBySearchCriteria(ImmutableRoutesSearchCriteria searchCriteria) {
+       return null;
+    }
+
+    public List<RouteInfo> getRoutesBySearchCriteriaAndWeather(ImmutableRoutesSearchCriteria searchCriteria) {
+        return null;
+    }
 }
