c7da43ff9e802be282527cfe6f5184539c0f2efc
rachel <rachelcope.m@gmail.com>
2022-08-31 09:34:53 -0600

    Per Parametization and boiler plate dev #4, I deleted the previosu routeInfo class, and created RouteIF to make Route immutable

---
+++ RouteSuggesterService/src/main/java/service/repo/RouteIF.java
@@ -0,0 +1,23 @@
+package service.repo;
+
+import org.immutables.value.Value;
+
+@Value.Immutable
+public interface RouteIF {
+    String getRouteName();
+    String getMountainName();
+    boolean isStandardRoute();
+    boolean isSnowRoute();
+    String getGrade();
+    String getTrailhead();
+    int getSummitElevation();
+    int getGain();
+    int getLength();
+    String getExposure();
+    String getRockfallPotential();
+    String getRouteFinding();
+    String getCommitment();
+    int getRoadDifficulty();
+    String getRouteUrl();
+    String getTrailheadUrl();
+}
